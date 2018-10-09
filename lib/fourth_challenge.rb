96379d8364f753b3e34bb6a31fe966a66d23facc
Grace Choi <oiograceoio@gmail.com>
2018-10-09 04:33:50 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -16,7 +16,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"}, #your hashes here!
-        {name: "Nurse", age: "4", attitude: "worried"}
+        {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
